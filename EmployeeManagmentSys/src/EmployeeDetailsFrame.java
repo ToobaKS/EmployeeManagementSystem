0818699fe727226854e62de6810b5fd0e84f3b5a
Tooba <tksheikh17@gmail.com>
2023-01-07 15:04:51 -0500

    Updated login page GUI. Created the employee details frame with notes, schedule and time cards. Created the notes frame to be functional.

---
+++ EmployeeManagmentSys/src/EmployeeDetailsFrame.java
@@ -0,0 +1,115 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EmployeeDetailsFrame extends JFrame implements View, ActionListener {
+
+    private JButton notes;
+    private JButton details;
+    private JButton schedule;
+    private JButton timeCards;
+    private JButton benefits;
+
+
+    private JLabel employeeName;
+
+
+    private JPanel detailsPage;
+    private JPanel notesPage;
+    private JPanel schedulePage;
+    private JPanel timeCardPage;
+    private JPanel benefitsPage;
+
+    private Model model;
+
+    private JPanel mainPanel;
+    private JPanel page;
+    private JPanel menuPanel;
+    private JButton create;
+    private JList notesList;
+    private JButton previousW;
+    private JButton nextW;
+    private JLabel week;
+    private JPanel sun;
+    private JPanel mon;
+    private JPanel tue;
+    private JPanel wed;
+    private JPanel thur;
+    private JPanel fri;
+    private JPanel sat;
+    private JTable table1;
+    private JComboBox comboBox1;
+    private JComboBox comboBox3;
+    private JButton filter;
+
+
+    //Constants
+    final static String DETAILS = "Employee Details";
+    final static String NOTESP = "Notes";
+    final static String SCHEDULEP = "Schedule";
+    final static String TIMEP = "Time Cards";
+    final static String BENEFITSP = "Benefits";
+
+    public EmployeeDetailsFrame(Model model) {
+        super("Employee Information");
+
+        this.model = model;
+
+        initMenu();
+        initPage();
+
+        this.add(mainPanel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600,500);
+        this.setVisible(true);
+        create.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new CreateNoteFrame("Written by: Tooba", "");
+            }
+        });
+    }
+    private void initMenu() {
+
+        employeeName.setText("Tooba");
+
+        details.addActionListener(this::showPage);
+        notes.addActionListener(this::showPage);
+        schedule.addActionListener(this::showPage);
+        timeCards.addActionListener(this::showPage);
+        benefits.addActionListener(this::showPage);
+    }
+
+    private void initPage() {
+        // adding components to the panel
+        page.add(detailsPage, DETAILS);
+        page.add(notesPage, NOTESP);
+        page.add(schedulePage, SCHEDULEP);
+        page.add(timeCardPage, TIMEP);
+        page.add(benefitsPage, BENEFITSP);
+    }
+
+    private void initSchedulePage() {
+    }
+
+    private void showPage(ActionEvent actionEvent) {
+        String command = actionEvent.getActionCommand();
+        ((CardLayout)page.getLayout()).show(page, command);
+
+    }
+
+    @Override
+    public void systemUpdate(String command, String info) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        EmployeeDetailsFrame start = new EmployeeDetailsFrame(new Model());
+    }
+}
